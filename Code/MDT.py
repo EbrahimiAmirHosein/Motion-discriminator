40353552476712ca5706c6946e2b1c06459399c1
AmirHosein Ebrahimi <35047853+EbrahimiAmirHosein@users.noreply.github.com>
2022-05-31 11:03:26 +0430

    Update MDT.py

--- Code/MDT.py
+++ Code/MDT.py
@@ -4,13 +4,9 @@ from Helper import *
 
  
 usrInfo = UserInfo()
-
 win , square_1 , square_2 = set_window(screen_size = [widthPix, heightPix] , color = 'black')
-
 Instruction(win,usrInfo)
-
 dot_stim = setDotStim(win,nDots,coherence,dotSize,speed,color,opacity)
-
-#training_phase(win,dot_stim,usrInfo,square_1,square_2)
+training_phase(win,dot_stim,usrInfo,square_1,square_2)
 test_phase(win,dot_stim,usrInfo,square_1,square_2)
 Finish(win)
