56453f38e364150e65327ccf034376fd93982e51
AmirHosein Ebrahimi <35047853+EbrahimiAmirHosein@users.noreply.github.com>
2022-09-20 20:38:06 +0430

    Update MDT.py

--- Code/MDT.py
+++ Code/MDT.py
@@ -1,12 +1,11 @@
 from psychopy import visual , core , event 
-from Parameters import*
+from Parameters import *
 from Utils import *
 
- 
-usrInfo = UserInfo()
-win , square_1 , square_2 = set_window(screen_size = [widthPix, heightPix] , color = 'black')
-Instruction(win,usrInfo)
-dot_stim = setDotStim(win,nDots,coherence,dotSize,speed,color,opacity)
-training_phase(win,dot_stim,usrInfo,square_1,square_2)
-test_phase(win,dot_stim,usrInfo,square_1,square_2)
+usrInfo , Fname = UserInfo() #getting userInfo
+win , square_1  , square_2 = set_window(screen_size = [widthPix, heightPix] , color = 'black') #preparing window
+Instruction(win,usrInfo) # wpop-up instructions
+dot_stim = setDotStim(win,nDots,coherence,dotSize,speed,color,opacity) #setting up dot stimuli
+training_phase(win,dot_stim,usrInfo,square_1,square_2,Fname) # training phase
+test_phase(win,dot_stim,usrInfo,square_1,square_2,Fname) # test phase
 Finish(win)
